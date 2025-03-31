e1ac97b94b5c632ddc8c3cff2d8af82c92777741
Susana Rueda <susanalrueda@uts.edu.co>
2025-03-30 23:28:43 -0500

    enrutador y controladores

---
+++ src/routes/categorias.routes.js
@@ -0,0 +1,11 @@
+import {Router} from "express";
+import { methodHTTP as categoriaController} from "../controllers/categoria.controllers.js";
+
+/* creamos el enrutador*/
+const router = Router();
+
+/*metodo http get, configuracion respuesta desde server */
+router.get("/", categoriaController.getCategorias)
+
+/* hacemos disponible al router en toda la aplicacion */
+export default router;
